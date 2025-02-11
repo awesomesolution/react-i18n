c33b616abc2344100853553d402a5e02ea03f092
awesomesolution <awesome.hsolution@gmail.com>
2025-02-11 18:25:52 +0530

    initial commit

---
+++ src/i18n/i18n.ts
@@ -0,0 +1,32 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+// Import your translation files
+import translationEN from './en/translation.json';
+import translationFR from './fr/translation.json';
+
+// Define the resources
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  fr: {
+    translation: translationFR,
+  },
+};
+
+i18n
+  .use(LanguageDetector) // Automatically detect user's language
+  .use(initReactI18next) // Bind React-i18next
+  .init({
+    resources,
+    lng: 'en', // Default language
+    fallbackLng: 'en', // Fallback language
+    interpolation: {
+      escapeValue: false, // React already escapes values
+    },
+    debug: true, // Enable for debugging
+  });
+
+export default i18n;
